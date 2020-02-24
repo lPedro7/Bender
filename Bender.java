2c4d3a630bdc3d52c27f9d0aaf8801ee767e696f
pedro <pedroribeirobatista1998@gmail.com>
2020-02-24 17:28:30 +0100

    modificado teleport

--- Bender.java
+++ Bender.java
@@ -9,6 +9,7 @@ class Bender {
     int[] posSalida;
     String resultado = "";
     LinkedList<Integer[]> teleport = new LinkedList<>();
+    LinkedList<Integer[]> iterator = new LinkedList<>();
 
     // Constructor: ens passen el mapa en forma d'String
     public Bender(String mapa) {
@@ -30,7 +31,11 @@ class Bender {
                 }
                 if (this.mapa[i][j].equals("T")) {
                     teleport.add(new Integer[]{i, j});
-                    System.out.println("There's a teleport !" + i + " " + j);
+                    System.out.println("There's a teleport ! " + i + " " + j);
+                }
+                if (this.mapa[i][j].equals("I")) {
+                    iterator.add(new Integer[]{i, j});
+                    System.out.println("There's a iterator!! " + i + " " + j);
                 }
             }
         }
@@ -44,105 +49,161 @@ class Bender {
 // segons la posició del robot a cada moment.
     public String run() {
 
-        while (true) {
+        while (p.currentPriority == p.priority1) {
 
             while (p.moveS(this.mapa) != null) {
-                this.resultado += p.lastMove;
+                move();
 
-                dibuixaMapa(this.mapa);
-
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
             }
+            if (p.currentPriority != p.priority1)break;
             while (p.moveE(this.mapa) != null) {
-                this.resultado += p.lastMove;
-
-                dibuixaMapa(this.mapa);
-
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
+                move();
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
             }
+            if (p.currentPriority != p.priority1)break;
 
             if (p.moveS(this.mapa) != null) {
 
-                this.resultado += p.lastMove;
-
-                dibuixaMapa(this.mapa);
-
-                if (isTeleport(p.currentPos)) {
-                   this.mapa = teletransportar(this.mapa);
-                }
+                move();
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
                 continue;
             }
+            if (p.currentPriority != p.priority1)break;
 
             while (p.moveN(this.mapa) != null) {
-                this.resultado += p.lastMove;
-
-                dibuixaMapa(this.mapa);
-
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
+                move();
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
             }
+            if (p.currentPriority != p.priority1)break;
 
             if (p.moveE(this.mapa) != null) {
-                this.resultado += p.lastMove;
-
-                dibuixaMapa(this.mapa);
-
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
+                move();
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
                 continue;
             }
+            if (p.currentPriority != p.priority1)break;
 
             if (p.moveS(this.mapa) != null) {
 
-                this.resultado += p.lastMove;
-
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
+                move();
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
                 continue;
             }
+            if (p.currentPriority != p.priority1)break;
 
 
             while (p.moveW(this.mapa) != null) {
-                this.resultado += p.lastMove;
 
+                move();
 
-                if (isTeleport(p.currentPos)) {
-                    this.mapa = teletransportar(this.mapa);
-                }
                 if (Arrays.equals(p.currentPos, posSalida)) {
                     return resultado;
                 }
             }
 
+        }
+        while (p.currentPriority == p.priority2) {
+
+            while (p.moveN(this.mapa) != null) {
+                move();
+
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+            }
+            if (p.currentPriority != p.priority2)break;
+
+            while (p.moveW(this.mapa) != null) {
+                move();
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+            }
+            if (p.currentPriority != p.priority2)break;
+
+            if (p.moveN(this.mapa) != null) {
+
+                move();
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+                continue;
+            }
+            if (p.currentPriority != p.priority2)break;
+
+            while (p.moveS(this.mapa) != null) {
+                move();
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+            }
+            if (p.currentPriority != p.priority2)break;
+
+            if (p.moveN(this.mapa) != null) {
+                move();
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+                continue;
+            }
+            if (p.currentPriority != p.priority2)break;
+
+            if (p.moveW(this.mapa) != null) {
+
+                move();
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+                continue;
+            }
+            if (p.currentPriority != p.priority2)break;
+
+
+            while (p.moveE(this.mapa) != null) {
+
+                move();
+
+                if (Arrays.equals(p.currentPos, posSalida)) {
+                    return resultado;
+                }
+            }
+            if (p.currentPriority != p.priority2)break;
 
         }
 
+        return resultado;
+    }
 
+
+
+
+    void move() {
+        this.resultado += p.lastMove;
+
+        dibuixaMapa(this.mapa);
+
+        if (isTeleport(p.currentPos)) {
+            this.mapa = teletransportar(this.mapa);
+        }
+
+        if (isIterator(p.currentPos)) {
+            p.changePriority();
+            System.out.println("Change priority !!");
+            System.out.println(p.currentPriority);
+        }
     }
 
     void dibuixaMapa(String[][] mapa) {
@@ -160,15 +221,48 @@ class Bender {
 
         System.out.println("teletransportamos !!");
 
-        LinkedList<Integer> distancia = new LinkedList<>();
-
-        int distPlayer = p.currentPos[0] + p.currentPos[1];
+        System.out.println("Buscamos Teleport mas cercano");
 
         for (int i = 0; i < teleport.size(); i++) {
-            distancia.add(teleport.get(i)[0] + teleport.get(i)[1]);
+            System.out.println(teleport.get(i)[0] + " - " + teleport.get(i)[1]);
+        }
+
+        System.out.println("Cual es mas cercano ?");
+
+        int[] tp = new int[2];
+
+        if (teleport.size() == 2) {
+
+            for (int i = 0; i < teleport.size(); i++) {
+
+                int[] tel1 = new int[]{teleport.get(i)[0], teleport.get(i)[1]};
+
+                if (!Arrays.equals(tel1, p.currentPos)) {
+                    tp = tel1;
+                }
+
+            }
+
+            p.currentPos = tp;
+
         }
 
         return mapa;
+    }
+
+    boolean isIterator(int[] coor) {
+
+        if (iterator.size() == 0) return false;
+
+        for (int i = 0; i < iterator.size(); i++) {
+
+            int[] it = new int[]{iterator.get(i)[0], iterator.get(i)[1]};
+
+            if (Arrays.equals(coor, it)) return true;
+
+        }
+
+        return false;
     }
 
     boolean isTeleport(int[] coor) {
@@ -278,6 +372,11 @@ class Player {
         return mapa;
     }
 
+    void changePriority() {
+        if (currentPriority == priority1) {
+            currentPriority = priority2;
+        } else currentPriority = priority1;
+    }
 
 }
 
